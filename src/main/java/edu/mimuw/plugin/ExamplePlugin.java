70ef29250be88eba996dbbee0c4377b86a0834e0
Mateusz Danowski <mateusz.danowski@syncron.com>
2025-06-24 13:36:29 +0200

    Print number of files for each project

--- src/main/java/edu/mimuw/plugin/ExamplePlugin.java
+++ src/main/java/edu/mimuw/plugin/ExamplePlugin.java
@@ -1,5 +1,7 @@
 package edu.mimuw.plugin;
 
+import java.util.List;
+
 import edu.mimuw.sovaide.domain.model.Project;
 import edu.mimuw.sovaide.domain.plugin.PluginSova;
 import edu.mimuw.sovaide.domain.repository.ProjectRepository;
@@ -12,5 +14,11 @@ public class ExamplePlugin implements PluginSova {
 		Project project = new Project();
 		project.setName("Project created by a plugin!");
 		repository.save(project);
+
+		List<Project> allProjects = repository.findAll();
+		System.out.println("All projects in the repository:");
+		for (Project p : allProjects) {
+			System.out.println(" - " + p.getName() + " (ID: " + p.getId() + ") with " + p.getFiles().size() + " files");
+		}
 	}
 }
